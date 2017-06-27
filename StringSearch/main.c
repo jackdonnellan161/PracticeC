afdfdd4c75997f88d822e249be64ce0f54e6ed50
jackdonnellan161 <jackdonnellan161@gmail.com>
2017-06-27 00:25:30 -0400

    Done for tonight, move on with using null.

--- StringSearch/main.c
+++ StringSearch/main.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 #include "func.c"
 
-int main() {
-	/*Comment*/
-	printf("Hello World")
+int main(int argc, char *argv[]) {
+	/*Set Variables*/
+
+	/*Check for correct number of arguements*/
+	if(argc > 3 || argc < 2){
+		printf("Incorrect number of arguments\n");
+	}
+
+	printf("%s %lu  %s\n",argv[1],(sizeof(argv[1])/sizeof(argv[1][0])),argv[2]);
+
+	/*for(int ii=0; ii<sizeof(ar*/
 	return 0;
 }
+
